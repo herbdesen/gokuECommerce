8da54317482450df2697fe00cd32cbe5ab6707ea
herbert.r.santos <herbert.r.santos@accenture.com>
2021-04-22 05:20:16 -0300

    Goku

--- src/main/java/com/goku/mapper/UserMapper.java
+++ src/main/java/com/goku/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package com.goku.mapper;
 
+import com.goku.api.model.RoleDTO;
 import com.goku.api.model.UserDTO;
+import com.goku.domain.Role;
 import com.goku.domain.User;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
@@ -14,4 +16,11 @@ public interface UserMapper {
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     UserDTO entityToDto(User entity, UserDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Role dtoToEntity(RoleDTO dto, Role entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    RoleDTO entityToDto(Role entity, RoleDTO dto);
+
 }
--- src/main/java/com/goku/mapper/impl/UserMapperImpl.java
+++ src/main/java/com/goku/mapper/impl/UserMapperImpl.java
@@ -1,9 +1,14 @@
 package com.goku.mapper.impl;
 
+import com.goku.api.model.RoleDTO;
 import com.goku.api.model.UserDTO;
+import com.goku.domain.Role;
 import com.goku.domain.User;
 import com.goku.mapper.UserMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserMapperImpl implements UserMapper {
@@ -17,6 +22,14 @@ public class UserMapperImpl implements UserMapper {
         entity.setPassword(dto.getPassword());
         entity.setFirstName(dto.getFirstName());
         entity.setLastName(dto.getLastName());
+
+        List<Role> roleList = new ArrayList<>();
+        for(RoleDTO roleDto : dto.getRoles()){
+            Role role = new Role();
+            roleList.add(dtoToEntity(roleDto, role));
+        }
+        entity.setRoles(roleList);
+
         return entity;
     }
 
@@ -30,6 +43,35 @@ public class UserMapperImpl implements UserMapper {
         dto.setPassword(entity.getPassword());
         dto.setFirstName(entity.getFirstName());
         dto.setLastName(entity.getLastName());
+
+        List<RoleDTO> roleDtoList = new ArrayList<>();
+        for(Role role : entity.getRoles()){
+            RoleDTO roleDto = new RoleDTO();
+            roleDtoList.add(entityToDto(role, roleDto));
+        }
+        dto.setRoles(roleDtoList);
+
+        return dto;
+    }
+
+    @Override
+    public Role dtoToEntity(RoleDTO dto, Role entity) {
+        if (dto == null || entity == null) {
+            return null;
+        }
+        entity.setRoleName(dto.getRoleName());
+        entity.setDescription(dto.getDescription());
+        return entity;
+    }
+
+    @Override
+    public RoleDTO entityToDto(Role entity, RoleDTO dto) {
+        if (dto == null || entity == null) {
+            return null;
+        }
+        dto.setId(entity.getId());
+        dto.setRoleName(entity.getRoleName());
+        dto.setDescription(entity.getDescription());
         return dto;
     }
 }
